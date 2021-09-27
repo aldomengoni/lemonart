e87dbd31ad34407a2496a3c83ad88b152858f26c
Ramon Caraballo <ramon.caraballo@growit.com.do>
2021-09-27 07:35:22 -0400

    [ADD] GrowIT Modules

---
+++ stock_landed_cost_extra/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Stock Landed Cost Extra",
+
+    'summary': """
+    
+    """,
+
+    'description': """
+    """,
+
+    'author': "GrowIT",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.2',
+
+    'depends': ['base', 'stock_landed_costs', 'report_xlsx_helper'],
+
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+        'security/ir.model.access.csv'
+    ],
+}
